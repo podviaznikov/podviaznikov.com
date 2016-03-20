3cd6cc6e9d302bf1a514ccb6c456e06bc9a51a47
Anton Podviaznikov <podviaznikov@gmail.com>
2016-03-19 21:17:38 -0700

    working on the landing page

--- src/com/podviaznikov/index.clj
+++ src/com/podviaznikov/index.clj
@@ -7,17 +7,33 @@
     [:head
       [:meta {:charset "utf-8"}]
       [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-      [:title "perun: composable static site generator build with Clojure and Boot"]
+      [:title "Anton Podviaznikov"]
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
-      [:meta {:itemprop "author" :name "author" :content "hashobject (team@hashobject.com)"}]
-      [:link {:rel "icon" :type "image/png" :href "/favicon-32x32.png" :sizes "32x32"}]
-      [:link {:rel "icon" :type "image/png" :href "/favicon-96x96.png" :sizes "96x96"}]
-      [:link {:rel "icon" :type "image/png" :href "/favicon-16x16.png" :sizes "16x16"}]
+      [:meta {:itemprop "author" :name "author" :content "Anton Podviaznikov"}]
       (include-css "/tufte.css")
       ]
     [:body
-      [:div
+      [:header
         [:h1 "Anton Podviaznikov"]
-        [:p "Software engineer"]]
+        [:p "software engineer, traveller, citizen of the world"]]
+      [:section
+        [:h2 "Geography"]
+          [:ul
+            [:li "I was born in Germany"]
+            [:li "I grew up in post-Soviet Ukraine"]
+            [:li "I left Ukraine for Montenegro"]
+            [:li "I visited more than 20 countries"]
+            [:li "I live in San Francisco now"]]
+        ]
+        [:section
+          [:h2 "Work"]
+            [:ul
+              [:li "I worked in decently large outsourcing company in Ukraine"]
+              [:li "I worked in small product company in Montenegro"]
+              [:li "I was freelance developer for two periods"]
+              [:li "I was founding engineer at CircuitHub"]
+              [:li "I was first engineer at HumanAPI"]
+              [:li "Right now I work at Runnable"]]
+          ]
 
       ]))
